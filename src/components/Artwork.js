dbe9bda12cf81719e5aebc34336ed418f9fb6f06
chrisdennett <chrisdennett@gmail.com>
2020-12-03 18:36:46 +0000

    added drag to gallery intro

--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -26,6 +26,22 @@ export const Artwork = ({
             initial="enter"
             animate="center"
             exit="exit"
+            transition={{
+              x: { type: "spring", stiffness: 300, damping: 30 },
+              opacity: { duration: 0.2 },
+            }}
+            drag="x"
+            dragConstraints={{ left: 0, right: 0 }}
+            dragElastic={1}
+            onDragEnd={(e, { offset, velocity }) => {
+              console.log("offset: ", offset);
+              const swipe = swipePower(offset.x, velocity.x);
+              if (swipe < -swipeConfidenceThreshold) {
+                onNext();
+              } else if (swipe > swipeConfidenceThreshold) {
+                onPrev();
+              }
+            }}
           >
             <GalleryIntro galleryId={galleryData.galleryId} />
           </PictureHolder>
@@ -47,6 +63,7 @@ export const Artwork = ({
             dragConstraints={{ left: 0, right: 0 }}
             dragElastic={1}
             onDragEnd={(e, { offset, velocity }) => {
+              console.log("offset: ", offset);
               const swipe = swipePower(offset.x, velocity.x);
 
               if (swipe < -swipeConfidenceThreshold) {
