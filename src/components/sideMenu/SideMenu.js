44f3c1abb1d85f14caf8acfe7d3bd1efee5ca276
chrisdennett <chrisdennett@gmail.com>
2021-01-16 12:53:02 +0000

    Side menu working

--- src/components/sideMenu/SideMenu.js
+++ src/components/sideMenu/SideMenu.js
@@ -1,22 +1,31 @@
 import { Link } from "@reach/router";
 import React from "react";
 import styles from "./sideMenu.module.css";
+import { exhibitionData } from "../../data/exhibitionData";
 
 export const SideMenu = ({ isOpen, onClose }) => {
   const visibility = isOpen ? styles.show : styles.hide;
 
+  const galleryLinks = exhibitionData.galleries.map((g) => {
+    return {
+      label: g.photographer,
+      link: `/${g.galleryId}`,
+    };
+  });
+
   return (
     <div className={`${styles.flyoutMenu} ${visibility}`} onClick={onClose}>
-      <p>Can you spot the item that doesn't belong?</p>
-      <ul>
+      <div className={styles.navList}>
         <Link to={"/"}>HOME</Link>
-        <li>Ipsum</li>
-        <li>Dolor</li>
-        <li>Sit</li>
-        <li>Bumblebees</li>
-        <li>Aenean</li>
-        <li>Consectetur</li>
-      </ul>
+
+        <h2 className={styles.menuHeader}>Photographers:</h2>
+
+        {galleryLinks.map((l) => (
+          <Link key={l.link} to={l.link}>
+            {l.label}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
