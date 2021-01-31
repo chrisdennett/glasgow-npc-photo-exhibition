bb8c4e442f784dfa674329d964e92582f3f60f89
chrisdennett <chrisdennett@gmail.com>
2021-01-31 14:37:10 +0000

    Added link to main GNPC site

--- src/components/OuterRoom.js
+++ src/components/OuterRoom.js
@@ -1,10 +1,19 @@
 import styled from "styled-components";
 import { HomeHeader } from "../components/HomeHeader";
 import { HomeFooter } from "../components/HomeFooter";
+import { ExternalLink } from "./ExternalLink";
 
 export const OuterRoom = ({ children }) => {
   return (
     <Page>
+      <TopLink>
+        <ExternalLink
+          url="https://www.glasgownationalparkcity.org/"
+          label="
+        Glasgow National Park City Main Site"
+        />
+      </TopLink>
+
       <HomeHeader>
         <h1>Glasgow National Park City</h1>
         <h2>Photo Exhibition</h2>
@@ -16,6 +25,20 @@ export const OuterRoom = ({ children }) => {
     </Page>
   );
 };
+
+const TopLink = styled.div`
+  background: #ce7c41;
+  position: absolute;
+  padding: 5px 10px;
+  border-radius: 10px;
+  top: 5px;
+  right: 5px;
+  a {
+    color: white;
+  }
+
+  box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.5);
+`;
 
 const Page = styled.div`
   background: linear-gradient(
